7187f5d17f35cdc25af0b50949b4c0b8a2eb507a
lingwey <nachozanetti0@gmail.com>
2022-01-27 00:22:50 -0300

    tarea 5
    practica de formulario

---
+++ formularo.js
@@ -0,0 +1,27 @@
+const $cargarDatos = document.querySelector('#cargar-datos');
+
+$cargarDatos.onclick = function (){
+    const primerNombre = document.querySelector('#primer-nombre-usuario');
+    const segundoNombre = document.querySelector('#segundo-nombre-usuario');
+    const apellidoUsuario = document.querySelector('#apellido-usuario');
+    const edadUsuario = document.querySelector('#edad-usuario');
+    const vienvenidaTitulo = document.querySelector('h1');
+    const principal = document.querySelector('body');
+
+    vienvenidaTitulo.textContent = "¡hola! " + primerNombre.value;
+
+   const divDePagina = document.createElement('div');
+   const parrafoEnDiv = document.createElement('p');
+   const parrafoEnDivDos = document.createElement('p');
+   const contenidoParrafo = document.createTextNode( "primer nombre: " + primerNombre.value + " segundo nombre: " + segundoNombre.value + 
+   " apellido: " + apellidoUsuario.value + " edad: " + edadUsuario.value);
+   //const contenidoParrafoDos = document.createTextNode ()
+
+   parrafoEnDiv.appendChild(contenidoParrafo);
+   divDePagina.appendChild(parrafoEnDiv);
+   principal.appendChild(divDePagina);
+
+
+    console.log("hola")
+    return false
+}
